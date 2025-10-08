c00d306b4304f3939cfc17f9ae2b6c1cdde2ef04
Evan Zazula <evan_zazula@me.com>
2025-10-08 13:12:18 -0700

    my neovim config

--- init.lua
+++ init.lua
@@ -89,7 +89,13 @@ P.S. You can delete this when you're done too. It's your config now! :)
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
+--
+-- EZ
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakat = ' \t;:,!?'
+--
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
@@ -881,11 +887,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'Mofiqul/dracula.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('dracula').setup {
+        transparent_bg = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -894,7 +901,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'dracula'
     end,
   },
 
