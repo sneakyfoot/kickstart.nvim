3bd35bd4201b6a2fda66d0d6a78a14409782f203
testName <47622407+sneakyfoot@users.noreply.github.com>
2026-01-05 23:17:04 -0800

    collapse

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -104,4 +104,15 @@ return {
     --   vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-dismiss)', { silent = true })
     -- end,
   },
+  {
+    'chrisgrieser/nvim-origami',
+    event = 'VeryLazy',
+    opts = {}, -- needed even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
 }
