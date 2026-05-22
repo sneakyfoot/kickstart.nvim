4e285f819f0ef0fff230542e520c509636351996
ez <evan_zazula@me.com>
2026-05-21 23:35:19 -0700

    drop mini.surround so 's' works as substitute in visual mode

--- init.lua
+++ init.lua
@@ -300,7 +300,6 @@ end, { desc = '[S]earch [N]eovim config' })
 -- ============================================================================
 
 require('mini.ai').setup({ n_lines = 500 })
-require('mini.surround').setup()
 local statusline = require('mini.statusline')
 statusline.setup({ use_icons = vim.g.have_nerd_font })
 ---@diagnostic disable-next-line: duplicate-set-field
